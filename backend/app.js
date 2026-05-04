3579593d25179acc6c8e323af7f9480c81a3e498
correa2004 <133184070+correa2004@users.noreply.github.com>
2026-05-04 12:15:17 -0500

    Add new route for clientes in app.js

--- backend/app.js
+++ backend/app.js
@@ -14,11 +14,15 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.use('/api/productos', require('./routes/productos'));
 app.use('/api/citas', require('./routes/citas'));
 
+// --- NUEVA RUTA PARA CLIENTES ---
+// Esto conecta el archivo que creamos en el Paso 3
+app.use('/api/clientes', require('./routes/clienteRoutes')); 
+
 // Home
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 app.listen(3000, () => {
-  console.log('🚀 http://localhost:3000');
+  console.log('🚀 Servidor corriendo en: http://localhost:3000');
 });
