db6b080de219c9eeef201a7631dcb63473a20f27
correa2004 <133184070+correa2004@users.noreply.github.com>
2026-05-04 21:00:42 -0500

    Fix route order for verificar/email endpoint

--- backend/routes/clienteRoutes.js
+++ backend/routes/clienteRoutes.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 const clienteController = require('../controllers/clienteController');
 
-// Definimos las rutas GET
+// ESTE VA PRIMERO
+router.get('/verificar/email', clienteController.verificarEmail);
+
+
 router.get('/', clienteController.obtenerClientes);
 router.get('/:id', clienteController.obtenerClientePorId);
-router.get('/verificar/email', clienteController.verificarEmail);
 
 module.exports = router;
